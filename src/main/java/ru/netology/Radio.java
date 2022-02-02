17e112560b7e3839539cedff86992df8ac426490
LaraKra <konlor@rambler.ru>
2022-02-02 19:45:49 +0300

    commit two new file

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+public class Radio {
+
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > 9) {
+            return;
+        }
+        if (currentStation < 0) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RadioTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"'CurrentStation equal nine', 9, 9",
+            "'CurrentStation more than', 10, 0",
+            "'CurrentStation between zero and nine', 4, 4",
+            "'CurrentStation equal zero', 0, 0",
+            "'CurrentStation less than zero', -1, 0"
+    })
+    void currentRadio(String testName, int setCurrentStation, int expected) {
+        Radio rad = new Radio();
+        rad.setCurrentStation(setCurrentStation);
+
+        int actual = rad.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+
+}
